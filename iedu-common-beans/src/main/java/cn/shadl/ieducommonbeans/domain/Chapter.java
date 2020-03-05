9c44b664c703c143633a23b4387744888667ddb1
ShadL-17 <ShadL-17@foxmail.com>
2020-03-05 21:38:01 +0800

    课程结构页样式更新

--- iedu-common-beans/src/main/java/cn/shadl/ieducommonbeans/domain/Chapter.java
+++ iedu-common-beans/src/main/java/cn/shadl/ieducommonbeans/domain/Chapter.java
@@ -2,10 +2,26 @@ package cn.shadl.ieducommonbeans.domain;
 
 import lombok.Data;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
 @Data
-public class Chapter {
+@Entity
+@Table(name = "chapter")
+public class Chapter implements Serializable, Cloneable {
+
+    @Id
     private Integer chid;//
+
+    @Column
     private String title;//
+
+    @Column
     private String description;//
+
+    @Column
     private Integer cid;//
 }
